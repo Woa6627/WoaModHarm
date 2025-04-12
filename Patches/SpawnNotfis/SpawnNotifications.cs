cf19de4307f2f391558284e0117af0831f42494f
Woa6627 <reecestephens99@gmail.com>
2025-04-13 00:22:25 +0100

    Added hopefully all enemies on spawn patchers

--- Patches/SpawnNotfis/SpawnNotifications.cs
+++ Patches/SpawnNotfis/SpawnNotifications.cs
@@ -6,7 +6,7 @@ public class NotifyGnomeSpawn
 {
     private static int gnomes = 0;
     [HarmonyPostfix, HarmonyPatch(nameof(EnemyGnome.OnSpawn))]
-    public static void OnGnomeSpawn(EnemyGnome __instance)
+    public static void OnEnemySpawnPatch(EnemyGnome __instance)
     {
         if(SemiFunc.IsMasterClientOrSingleplayer() && SemiFunc.EnemySpawn(__instance.enemy))
         {
@@ -20,7 +20,7 @@ public class NotifyGnomeSpawn
 public class NotifyFloaterSpawn
 {
     [HarmonyPostfix, HarmonyPatch(nameof(EnemyFloater.OnSpawn))]
-    public static void OnGnomeSpawn(EnemyFloater __instance)
+    public static void OnEnemySpawnPatch(EnemyFloater __instance)
     {
         if(SemiFunc.IsMasterClientOrSingleplayer() && SemiFunc.EnemySpawn(__instance.enemy))
         {
@@ -33,7 +33,7 @@ public class NotifyFloaterSpawn
 public class NotifyRobeSpawn
 {
     [HarmonyPostfix, HarmonyPatch(nameof(EnemyRobe.OnSpawn))]
-    public static void OnGnomeSpawn(EnemyRobe __instance)
+    public static void OnEnemySpawnPatch(EnemyRobe __instance)
     {
         if(SemiFunc.IsMasterClientOrSingleplayer() && SemiFunc.EnemySpawn(__instance.enemy))
         {
@@ -45,7 +45,7 @@ public class NotifyRobeSpawn
 public class NotifyHuntsManSpawn
 {
     [HarmonyPostfix, HarmonyPatch(nameof(EnemyHunter.OnSpawn))]
-    public static void OnGnomeSpawn(EnemyHunter __instance)
+    public static void OnEnemySpawnPatch(EnemyHunter __instance)
     {
         if(SemiFunc.IsMasterClientOrSingleplayer() && SemiFunc.EnemySpawn(__instance.enemy))
         {
@@ -58,7 +58,7 @@ public class NotifyHuntsManSpawn
 public class NotifySlowMouthSpawn
 {
     [HarmonyPostfix, HarmonyPatch(nameof(EnemySlowMouth.OnSpawn))]
-    public static void OnGnomeSpawn(EnemySlowMouth __instance)
+    public static void OnEnemySpawnPatch(EnemySlowMouth __instance)
     {
         if(SemiFunc.IsMasterClientOrSingleplayer() && SemiFunc.EnemySpawn(__instance.enemy))
         {
@@ -71,7 +71,7 @@ public class NotifySlowMouthSpawn
 public class NotifyAnimalSpawn
 {
     [HarmonyPostfix, HarmonyPatch(nameof(EnemyAnimal.OnSpawn))]
-    public static void OnGnomeSpawn(EnemyAnimal __instance)
+    public static void OnEnemySpawnPatch(EnemyAnimal __instance)
     {
         if(SemiFunc.IsMasterClientOrSingleplayer() && SemiFunc.EnemySpawn(__instance.enemy))
         {
@@ -84,7 +84,7 @@ public class NotifyAnimalSpawn
 public class NotifyHiddenSpawn
 {
     [HarmonyPostfix, HarmonyPatch(nameof(EnemyHidden.OnSpawn))]
-    public static void OnHiddenSpawn(EnemyHidden __instance)
+    public static void OnEnemySpawnPatch(EnemyHidden __instance)
     {
         if(SemiFunc.IsMasterClientOrSingleplayer() && SemiFunc.EnemySpawn(__instance.enemy))
         {
@@ -97,11 +97,115 @@ public class NotifyHiddenSpawn
 public class NotifyHeadmanSpawn
 {
     [HarmonyPostfix, HarmonyPatch(nameof(EnemyHeadController.OnSpawn))]
-    public static void OnHeadManSpawn(EnemyHeadController __instance)
+    public static void OnEnemySpawnPatch(EnemyHeadController __instance)
     {
         if(SemiFunc.IsMasterClientOrSingleplayer())
         {
             Debug.Log("HeadMan Spawned");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(EnemyUpscream))]
+public class NotifyUpScreamSpawn
+{
+    [HarmonyPostfix, HarmonyPatch(nameof(EnemyUpscream.OnSpawn))]
+    public static void OnEnemySpawnPatch(EnemyHeadController __instance)
+    {
+        if(SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            Debug.Log("UpScream Spawned");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(EnemyDuck))]
+public class NotifyDuckSpawn
+{
+    [HarmonyPostfix, HarmonyPatch(nameof(EnemyDuck.OnSpawn))]
+    public static void OnEnemySpawnPatch(EnemyHeadController __instance)
+    {
+        if(SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            Debug.Log("Duck Spawned");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(EnemyCeilingEye))]
+public class NotifyCeilingEyeSpawn
+{
+    [HarmonyPostfix, HarmonyPatch(nameof(EnemyCeilingEye.OnSpawn))]
+    public static void OnEnemySpawnPatch(EnemyHeadController __instance)
+    {
+        if(SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            Debug.Log("Ceiling Eye Spawned");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(EnemyThinMan))]
+public class NotifyThinManSpawn
+{
+    [HarmonyPostfix, HarmonyPatch(nameof(EnemyThinMan.OnSpawn))]
+    public static void OnEnemySpawnPatch(EnemyHeadController __instance)
+    {
+        if(SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            Debug.Log("ThinMan Spawned");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(EnemyBang))]
+public class NotifyBangSpawn
+{
+    [HarmonyPostfix, HarmonyPatch(nameof(EnemyBang.OnSpawn))]
+    public static void OnEnemySpawnPatch(EnemyHeadController __instance)
+    {
+        if(SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            Debug.Log("Bang Spawned");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(EnemyBeamer))]
+public class NotifyBeamerSpawn
+{
+    [HarmonyPostfix, HarmonyPatch(nameof(EnemyBang.OnSpawn))]
+    public static void OnEnemySpawnPatch(EnemyHeadController __instance)
+    {
+        if(SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            Debug.Log("Beamer Spawned");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(EnemyBowtie))]
+public class NotifyBowtieSpawn
+{
+    [HarmonyPostfix, HarmonyPatch(nameof(EnemyBowtie.OnSpawn))]
+    public static void OnEnemySpawnPatch(EnemyHeadController __instance)
+    {
+        if(SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            Debug.Log("Bowtie Spawned");
+        }
+    }
+}
+
+[HarmonyPatch(typeof(EnemyTumbler))]
+public class NotifyTumberSpawn
+{
+    [HarmonyPostfix, HarmonyPatch(nameof(EnemyTumbler.OnSpawn))]
+    public static void OnEnemySpawnPatch(EnemyHeadController __instance)
+    {
+        if(SemiFunc.IsMasterClientOrSingleplayer())
+        {
+            Debug.Log("Tumbler Spawned");
         }
     }
 }
--- obj/Debug/netstandard2.1/WoaModHarm.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/WoaModHarm.AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Woa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.5.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.5+81770e3f870a00cf3be40e75492c240efe084519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.5+ac56b36743c445603c168693144b2902e168afca")]
 [assembly: System.Reflection.AssemblyProductAttribute("WoaModHarm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WoaModHarm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.5.0")]
