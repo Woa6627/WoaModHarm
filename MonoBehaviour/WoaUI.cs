cbbd026090963b22924bbd8b62ddefcbca977da1
Woa6627 <reecestephens99@gmail.com>
2025-04-25 01:51:35 +0100

    Added debug ui

--- MonoBehaviour/WoaUI.cs
+++ MonoBehaviour/WoaUI.cs
@@ -17,10 +17,11 @@ public class WoaUI : MonoBehaviour
         PlayerController player = FindAnyObjectByType<PlayerController>();
         GUI.Box(new Rect(20, 20,200, 120), "Woa UI");
         if(player != null)
-            GUI.Label(new Rect(40, 30, 200, 20), $"is Moving {player.moving}");
+            GUI.Label(new Rect(50, 30, 200, 20), $"is Moving {player.moving}");
+        string speed = GUI.TextArea(new Rect(40, 10, 200, 20),"Speed", 20, GUIStyle.none);
         if(GUI.Button(new Rect(40, 60, 160, 30), "Test"))
         {
-            Debug.Log("Button Clicked");
+            if(player) player.SprintSpeed = float.Parse(speed);
         }
         if(GUI.Button(new Rect(40, 95, 160, 20), "Exit"))
         {
